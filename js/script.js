047d774ef2b40daf108a322c285ddab874856adc
alesildsantos <87341132+alesildsantos@users.noreply.github.com>
2021-09-13 15:57:25 -0300

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-var d = new Date();
-var date = d.getDate();
-var year = d.getFullYear();
-var month = d.getMonth();
-var monthArr = ["Janeiro", "Fevereiro","Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro"];
-month = monthArr[month];
-document.getElementById("date").innerHTML=date+"/"+month+"/"+year;
