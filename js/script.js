9edec536b34be5846207068d1a68d4dd3146c91d
alesildsantos <87341132+alesildsantos@users.noreply.github.com>
2021-09-13 15:59:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 
 var d = new Date();
+var wDay = d.getDay();
+var wDayArr = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+wDay = wDayArr[wDay];
 var date = d.getDate();
 var year = d.getFullYear();
 var month = d.getMonth();
-var monthArr = ["Janeiro", "Fevereiro","Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro"];
+var monthArr = ["Jan", "Fev","Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov","Dez"];
 month = monthArr[month];
-document.getElementById("date").innerHTML=date+"/"+month+"/"+year;
+document.getElementById("date").innerHTML=wDay+", "+date+" / "+month+" / "+year;
